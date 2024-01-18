4e817e777c86353033c1315f2758082abf2f68f9
Delgorithm <delgorithm@gmail.com>
2024-01-18 15:35:16 +0100

    feature : Google authentification allowed

--- Client/src/Auth/Firebase.jsx
+++ Client/src/Auth/Firebase.jsx
@@ -28,6 +28,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
+
 // Realtime database
 const db = getDatabase(app);
 const userDataRef = ref(db, 'users/');
--- Client/src/Auth/Signup.jsx
+++ Client/src/Auth/Signup.jsx
@@ -1,11 +1,14 @@
 // Main imports for authentification
 import React, { useEffect, useState } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 import { UserAuth } from '../Context/AuthContext';
 import { userDataRef, dbFireStore, app } from './Firebase';
 import { set, push } from 'firebase/database';
 import { collection, addDoc } from 'firebase/firestore';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { auth } from './Firebase';
+// Google authentification
+import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 
 // Icon for design
 import { BsCheck2Circle } from "react-icons/bs";
@@ -20,6 +23,7 @@ const Signup = () => {
   const [error, setError] = useState('');
   const navigate = useNavigate();
   const { createUserAccount } = UserAuth();
+  const provider = new GoogleAuthProvider();
 
   // Récupérer l'instance d'authentification
   const auth = getAuth();
@@ -50,6 +54,42 @@ const Signup = () => {
     } catch (e) {
       setError(e.message);
       console.log(e.message);
+    }
+  };
+  
+
+  // Auth with Google
+  const handleGoogleAuth = async (e) => {
+    e.preventDefault();
+    setError('');
+  
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+
+      const uid = user.uid;
+      const email = user.email
+
+      const userRef = push(userDataRef);
+      set(userRef, {
+        email: email,
+        uid: uid,
+      })
+
+      const docRef = await addDoc(collection(dbFireStore, "users"), {
+        email: email,
+        uid: uid,
+      });
+
+      navigate('/compte');
+
+    } catch(error) {
+        const errorCocde = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
     }
   };
 
@@ -83,17 +123,21 @@ const Signup = () => {
             <p>Stockage cloud limité à <span className='text-[#9f54ff] font-semibold'>100 Mo</span></p>
           </div>
           <span className='w-full h-[0.5px] bg-[#969696]'></span>
-          <p>Explorez d'autres formules. <span className='font-semibold'>Pro</span>, <span className='font-semibold'>Premium</span> :</p>
+          <p>Explorez d'autres plans: <span className='font-semibold'>Pro</span>, <span className='font-semibold'>Premium</span> :</p>
           <a href="" className='text-[#9f54ff] font-semibold hover:underline w-1/2'>Voir les différentes formules</a>
         </section>
-        {/* Left part : Will include the main part of the free plan with some tokens allowed */}
       </section>
-      <section className='bg-[#f0f0f0] m-10 h-3/6 w-[28%] rounded-3xl shadow-sm'>
+
+
+
+      <section className='bg-[#f0f0f0] m-10 h-[55%] w-[28%] rounded-3xl shadow-sm'>
         <div className='bg-white m-1.5 p-6 h-[90%] rounded-3xl'>
           <div className='flex flex-col justify-center items-center mt-5'>
-            <picture className='pb-5'>
-              <img src="./Img/logo-big.svg" alt="MyGeniusPDF's logo" />
-            </picture>
+            <Link to='/' className='pb-5'>
+              <picture>
+                <img src="./Img/logo-big.svg" alt="MyGeniusPDF's logo" />
+              </picture>
+            </Link>
             <h1 className='flex justify-center font-semibold text-2xl'>S'inscrire</h1>
             <p className='text-[#969696] text-sm text-center pb-4'>Inscrivez-vous pour bénéficier des avantages</p>
           </div>
@@ -137,13 +181,14 @@ const Signup = () => {
               onClick={handleSubmit}
               className='w-full bg-black text-white p-2 mt-4 rounded-lg hover:opacity-80 active:translate-y-0.5'
             >
+
               S'enregistrer
             </button>
 
             {/* Other option for registration */}
-            <div className='flex flex-col justify-center items-center mt-4'>
-              <p className='text-sm pb-2 text-[#969696]'>Ou continuer avec</p>
-              <button className='flex justify-center items-center p-1 text-3xl w-24 rounded-lg border border-[#f5f5f5] shadow-lg hover:opacity-70'>
+            <div className='flex flex-col justify-center items-center mt-5'>
+              <p className='text-sm mb-4 text-[#969696]'>Ou continuer avec</p>
+              <button onClick={handleGoogleAuth} className='flex justify-center items-center p-1 text-3xl w-24 rounded-lg border border-[#f5f5f5] shadow-lg hover:opacity-70'>
                 <FcGoogle />
               </button>
             </div>
@@ -160,7 +205,7 @@ const Signup = () => {
       {/* Styling img */}
       <picture>
         <img src="./Img/ellipse.svg" alt="" className='absolute top-0 left-0' />
-        <img src="./Img/gridbackground.svg" alt="" className='absolute top-0 left-0 opacity-30 -z-50' />
+        <img src="./Img/gridbackground.svg" alt="" className='absolute top-0 left-0  -z-50' />
       </picture>
     </main>
   );
